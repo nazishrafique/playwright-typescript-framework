c7dfde45c9bd51602f0f05da0d414310674fbad8
Nazish Rafique <nazishrafique16@gmail.com>
2026-09-07 01:13:47 +0500

    Add invalid login test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -20,3 +20,27 @@ test('User can login with valid credentials', async ({ page }) => {
     page.getByRole('heading', { name: 'Dashboard' })
   ).toBeVisible();
 });
+
+
+test('User cannot login with invalid credentials', async ({ page }) => {
+
+  // Open OrangeHRM
+  await page.goto('https://opensource-demo.orangehrmlive.com/');
+
+  // Enter username
+  await page.getByPlaceholder('Username').fill('Admin');
+
+  // Enter invalid password
+  await page.getByPlaceholder('Password').fill('ADMIN6767');
+
+  // Click Login
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  // Verify login failed
+  await expect(
+    page.getByRole('alert')
+  ).toHaveText('Invalid credentials');
+
+  // Verify user remains on login page
+  await expect(page).toHaveURL(/auth\/login/);
+});
